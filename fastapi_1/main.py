6f5e50a2f43976bcc1b140f3a63550046b57514f
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-05-07 13:44:17 +0530

    made changes

--- fastapi_1/main.py
+++ fastapi_1/main.py
@@ -43,6 +43,45 @@ def create_user(user:UserCreate, db:Annotated[Session, Depends(get_db)]):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Username already exists"
         )
+    result = db.execute(select(models.User).where(models.User.email==user.email))
+    existing_email = result.scalars().first()
+    if existing_email:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Email already registered'
+        )
+    new_user = models.User(
+        username = user.username,
+        email    = user.email
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/api/users/{user_id}',response_model=UserResponse)
+def get_user(user_id:int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id==user_id))
+    user = result.scalars().first()
+    if user:
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User not found"
+    )
+
+@app.get("/api/users/{user_id}/posts",response_model=list[PostResponse])
+def get_user_posts(user_id:int, db:Annotated[Session, Depends(get_db)]):
+    results =db.execute(select(models.User).where(models.User.id==user_id))
+    user    = results.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+    results = db.execute(select(models.Post).where(models.Post.user_id==user_id))
+    posts   = results.scalars().all()
+    return posts
 
 #POST CONTROLLERS:
 @app.get('/api/posts',response_model = list[PostResponse])
