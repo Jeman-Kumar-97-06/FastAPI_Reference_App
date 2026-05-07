4373ab4a0d3224b787c6ae74a544a5712781a325
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-05-07 09:30:12 +0530

    made changes

--- fastapi_1/schemas.py
+++ fastapi_1/schemas.py
@@ -3,7 +3,20 @@ This file is for data validation.
 This file says : What format of 'Post' should i expect from client side and What format of response to send.
 '''
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config=ConfigDict(from_attributes=True)
+    id:int
+    image_file:str | None
+    image_path:str
 
 class PostBase(BaseModel):
     title:str = Field(min_length=1, max_length=100)
