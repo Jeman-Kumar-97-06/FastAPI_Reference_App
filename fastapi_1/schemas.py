de55dc6e63599e5fc7f388ece63b0d2cd5e587ef
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2026-06-04 18:52:05 +0530

    made changes

--- fastapi_1/schemas.py
+++ fastapi_1/schemas.py
@@ -7,18 +7,32 @@ class UserBase(BaseModel):
     #password not yet
 
 class UserCreate(UserBase):
-    pass
+    password:str = Field(min_length=8)
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id:int
+    username:str
     image_file:str|None
     image_path:str
+
+class UserPrivate(UserPublic):
+    email: EmailStr
+
+# class UserResponse(UserBase):
+#     model_config = ConfigDict(from_attributes=True)
+#     id:int
+#     image_file:str|None
+#     image_path:str
 
 class UserUpdate(BaseModel):
     username   : str|None      = Field(default=None, min_length=1, max_length=50)
     email      : EmailStr|None = Field(default=None, max_length=120) 
     image_file : str|None      = Field(default=None, min_length=1, max_length=200)   
+
+class Token(BaseModel):
+    access_token:str
+    token_type:str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -38,7 +52,7 @@ class PostResponse(PostBase):
     id:int
     # --------------------------------
     user_id:int
-    author:UserResponse
+    author:UserPublic
     # --------------------------------
-    data_posted:datetime
+    date_posted:datetime
 
